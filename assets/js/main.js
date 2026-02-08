c83f960ce7fb4192de3a7493eb33be174ff43e2d
ryanluis-byte <ryanluispelayo17@gmail.com>
2026-02-08 09:23:21 +0800

    PortfolioUpdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: SnapFolio
-* Template URL: https://bootstrapmade.com/snapfolio-bootstrap-portfolio-template/
-* Updated: Jul 21 2025 with Bootstrap v5.3.7
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 
 (function() {
   "use strict";
